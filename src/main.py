791ced5f41f06dc6c3e7d2fdf9cb22e093aff7ad
Yavuz Karaca <yavkaraca@gmail.com>
2025-09-07 05:06:06 +0200

    random transform for fake occlusion added

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 from src.tasks.attraction import (generate_color_attraction, generate_size_attraction, generate_repulsion_gun,
                                   generate_repulsion_ambiguous, generate_gravity, generate_float)
 from src.tasks.expansion import generate_star_expansion_single_step, generate_star_expansion_full
-from src.tasks.occlusion import generate_occlusion_reversal
+from src.tasks.occlusion import generate_occlusion_reversal, generate_occlusion_transform
 from src.visualize import render_save_grid, render_save_combined_grids
-from src.util import next_run_dir, next_run_idx
+from src.util import next_run_idx
 
 
 def _save(base, name, idx, input_grid, output_grid):
@@ -16,6 +16,7 @@ def _save(base, name, idx, input_grid, output_grid):
 def main(N):
     tasks = [
         ("occlusion_reversal", generate_occlusion_reversal),
+        ("occlusion_transform", generate_occlusion_transform),
         ("attraction_color", generate_color_attraction),
         ("attraction_size", generate_size_attraction),
         ("attraction_gravity", generate_gravity),
